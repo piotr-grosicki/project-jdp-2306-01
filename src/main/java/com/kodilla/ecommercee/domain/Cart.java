61c0d79e5b8d3cc97b84f81a14c5ffc400863e95
ZuzannJanik <zjanik89@gmail.com>
2023-06-03 09:16:12 +0800

    JDP230601-15

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,7 @@
+package com.kodilla.ecommercee.domain;
+
+import javax.persistence.Entity;
+
+@Entity(name = "CARTS")
+public class Cart {
+}
--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,4 +1,45 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.*;
+
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+@Entity(name = "USERS")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    @Column(name = "USER_NAME")
+    private String userName;
+
+    @Column(name = "USER_TOKEN")
+    private String userToken;
+
+    @Column(name = "USER_TOKEN_VALID")
+    private LocalDate userTokenValid;
+
+    @Column(name = "USER_BLOCKED")
+    private boolean isUserBlocked;
+
+    public User (String userName, String userToken, LocalDate userTokenValid, boolean isUserBlocked) {
+        this.userName = userName;
+        this.userToken = userToken;
+        this.userTokenValid = userTokenValid;
+        this.isUserBlocked = isUserBlocked;
+    }
+
+    @OneToMany(targetEntity = Cart.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY)
+    public List<Cart> cartsList = new ArrayList();
 }
