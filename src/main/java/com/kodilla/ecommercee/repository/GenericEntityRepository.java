4d5748bec5d2333dd2081ef089fe459aa43a34a3
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-06-03 11:43:02 +0200

    Changes to test remote build

--- src/main/java/com/kodilla/ecommercee/repository/GenericEntityRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GenericEntityRepository.java
@@ -1,8 +1,8 @@
 package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.GenericEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GenericEntityRepository extends JpaRepository<GenericEntity, Long> { }
+public interface GenericEntityRepository extends CrudRepository<GenericEntity, Long> { }
