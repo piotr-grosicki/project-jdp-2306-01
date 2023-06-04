3ec506cf512b2c0da01a55f3deb53b677e0d0f9d
ZuzannJanik <zjanik89@gmail.com>
2023-06-04 10:34:37 +0800

    Fix .circleci

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -5,19 +5,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1/users")
 @CrossOrigin("*")
 @RequiredArgsConstructor
 public class UserController {
+    @GetMapping
+    public List<UserDto> getUsers() {
+        return new ArrayList<>();
+    }
+    @GetMapping(value = "{userId}")
+    public UserDto getUser(@PathVariable Long orderId) {
+        return new UserDto(1L);
+    }
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createUser(UserDto userDto) {
     }
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public UserDto blockUser(UserDto userDto) {
-        return new UserDto();
+        return new UserDto(1L);
     }
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void generateToken(UserDto userDto) {
+    }
+    @DeleteMapping
+    public  void deleteUser(Long userId) {
     }
 }
