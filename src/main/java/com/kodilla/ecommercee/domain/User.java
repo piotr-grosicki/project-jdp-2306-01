b21af8de3ec21aa52bf27c372db3f704b14c7a6d
ZuzannJanik <zjanik89@gmail.com>
2023-06-08 18:16:44 +0800

    JDP230601 conflict fix

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -18,29 +18,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
 
-    @Column(name = "USER_NAME")
-    private String userName;
 
-    @Column(name = "USER_TOKEN")
-    private String userToken;
-
-    @Column(name = "USER_TOKEN_VALID")
-    private LocalDate userTokenValid;
-
-    @Column(name = "USER_BLOCKED")
-    private boolean isUserBlocked;
-
-    public User (String userName, String userToken, LocalDate userTokenValid, boolean isUserBlocked) {
-        this.userName = userName;
-        this.userToken = userToken;
-        this.userTokenValid = userTokenValid;
-        this.isUserBlocked = isUserBlocked;
-    }
-
-    @OneToMany(targetEntity = Cart.class,
-            mappedBy = "user",
-            cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY)
-    public List<Cart> cartsList = new ArrayList();
 }
 
