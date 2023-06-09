2f6308888bceda33f79963970750d011c2ec3373
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-06-09 17:38:38 +0200

    JDP230601-13 Encja Group

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,7 +1,17 @@
 package com.kodilla.ecommercee.domain;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Data
+@AllArgsConstructor
 @Entity(name = "PRODUCT_GROUPS")
 public class Group {
     @Id
@@ -9,4 +19,16 @@ public class Group {
     @Column(name = "GROUP_ID")
     private Long groupId;
 
+    @Column(name = "GROUP_NAME")
+    private String groupName;
+
+    @OneToMany
+    @JoinTable(
+            name="CUST_PHONE",
+            joinColumns=
+            @JoinColumn(name="GROUP_ID", referencedColumnName="GROUP_ID"),
+            inverseJoinColumns=
+            @JoinColumn(name="PRODUCT_ID", referencedColumnName="PRODUCT_ID")
+    )
+    private List<Product> productList;
 }
