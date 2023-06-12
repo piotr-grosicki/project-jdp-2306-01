e0e2c00089b6cc4cf114b830dca9075967e7f03b
Mateusz <122115887+pawelecmateusz@users.noreply.github.com>
2023-06-12 11:16:17 +0200

    OrderRepository changes - Repository annotation - deletyById method - id > orderId parameter

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -2,18 +2,23 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.Order;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
     @Override
-    Optional<Order> findById(Long id);
+    Optional<Order> findById(Long orderId);
 
     @Override
     List<Order> findAll();
 
     @Override
     Order save(Order order);
+
+    @Override
+    void deleteById(Long orderId);
 }
