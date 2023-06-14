c019c10886c093a6110f0e488f3f6253104d8b6d
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-06-14 16:53:57 +0200

    Optional

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -4,7 +4,9 @@ import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GroupRepository extends CrudRepository<Group, Long> {
-    Group findByGroupName(String name);
+    Optional<Group> findByGroupName(String name);
 }
