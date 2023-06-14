c45d4babb799b7d70c1e6640e7c849833c582311
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-06-14 17:28:24 +0200

    Optional

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -4,9 +4,24 @@ import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GroupRepository extends CrudRepository<Group, Long> {
+    @Override
+    List<Group> findAll();
+
+    @Override
+    Group save(Group group);
+
+    @Override
+    Optional<Group> findById(Long groupId);
+
+    @Override
+    void deleteById(Long groupId);
+
+    @Override
+    void deleteAll();
     Optional<Group> findByGroupName(String name);
 }
