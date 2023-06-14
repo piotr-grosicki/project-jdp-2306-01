8788f85e897e6dbbaf96ca38aeb6ba150d4cab2b
Mateusz <122115887+pawelecmateusz@users.noreply.github.com>
2023-06-14 18:46:27 +0200

    Rename CartEntityTest

--- src/test/java/com/kodilla/ecommercee/CartEntityTest.java
+++ src/test/java/com/kodilla/ecommercee/CartEntityTest.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class CartEntityTestSuite {
+public class CartEntityTest {
 
     @Autowired
     private UserRepository userRepository;
