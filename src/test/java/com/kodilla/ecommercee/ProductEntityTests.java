e4f06803c9718d03741402d6dab88d6381c3a63b
Piotr Janczak <piotr.janczak1112@gmail.com>
2023-06-18 19:04:31 +0200

    ProductEntityTests v2.0

--- src/test/java/com/kodilla/ecommercee/ProductEntityTests.java
+++ src/test/java/com/kodilla/ecommercee/ProductEntityTests.java
@@ -1,19 +1,17 @@
 package com.kodilla.ecommercee;
 
-import com.kodilla.ecommercee.domain.Cart;
-import com.kodilla.ecommercee.domain.Group;
-import com.kodilla.ecommercee.domain.Product;
-import com.kodilla.ecommercee.repository.CartRepository;
-import com.kodilla.ecommercee.repository.GroupRepository;
-import com.kodilla.ecommercee.repository.ProductRepository;
+import com.kodilla.ecommercee.domain.*;
+import com.kodilla.ecommercee.repository.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
 @SpringBootTest
 public class ProductEntityTests {
 
@@ -23,33 +21,167 @@ public class ProductEntityTests {
     ProductRepository productRepository;
     @Autowired
     GroupRepository groupRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @Test
-    public void productGroupRelationsTest() {
-
+    public void productAndGroupRelationInitializing() {
         //Given
-        Group group1 = Group.builder()
-                .groupName("group1")
+        Group group = Group.builder()
+                .groupName("test group")
                 .build();
 
-        Product product2 = Product.builder()
-                .productName("test product 2")
-                .group(group1)
+        Product product = Product.builder()
+                .productName("test product")
+                .group(group)
                 .build();
 
-        group1.getProductList().add(product2);
+        group.getProductList().add(product);
 
         //When
-        groupRepository.save(group1);
+        groupRepository.save(group);
 
         //Then
-        assertFalse(productRepository.findAll().isEmpty());
+        assertTrue(productRepository.findByProductName("test product").isPresent());
+        assertTrue(groupRepository.findByGroupName("test group").isPresent());
         assertEquals(1, productRepository.findAll().size());
-        assertEquals(product2.getProductName(), productRepository.findAll().get(0).getProductName());
-
-        //Cleanup
-        productRepository.deleteAll();
+        assertEquals(1, groupRepository.findByGroupName("test group").get().getProductList().size());
     }
 
+    @Test
+    public void productAndCartRelationInitializing() {
+        //Given
+        Group group = Group.builder()
+                .groupName("test group")
+                .build();
 
+        Product product = Product.builder()
+                .productName("test product")
+                .group(group)
+                .build();
+
+        User user = User.builder()
+                .userName("test user")
+                .build();
+
+        Cart cart = Cart.builder()
+                .user(user)
+                .build();
+
+        group.getProductList().add(product);
+
+        //When
+        Group savedGroup = groupRepository.save(group);
+        user.cartList.add(cart);
+        cart.getProductList().add(product);
+        savedGroup.getProductList().get(0).getCartList().add(cart);
+        userRepository.save(user);
+        cartRepository.save(cart);
+
+        //Then
+        assertEquals(1, productRepository.findAll().size());
+        assertEquals(1, cartRepository.findAll().get(0).getProductList().size());
+        assertTrue(productRepository.findByProductName("test product").isPresent());
+        assertEquals(productRepository.findByProductName("test product"), Optional.of(cartRepository.findAll().get(0).getProductList().get(0)));
+    }
+
+    @Test
+    public void deletingProductDoesntDeleteGroup() {
+        //Given
+        Group group = Group.builder()
+                .groupName("test group")
+                .build();
+
+        Product product = Product.builder()
+                .productName("test product")
+                .group(group)
+                .build();
+
+        group.getProductList().add(product);
+        groupRepository.save(group);
+
+        //When
+        productRepository.deleteAll();
+
+        //Then
+        assertTrue(groupRepository.findByGroupName("test group").isPresent());
+        assertTrue(productRepository.findAll().isEmpty());
+        assertEquals(0, groupRepository.findByGroupName("test group").get().getProductList().size());
+    }
+
+    @Test
+    public void deletingProductDoesntDeleteCartNorUserButUpdatesCartsProductList() {
+        //Given
+        Group group = Group.builder()
+                .groupName("test group")
+                .build();
+
+        Product product = Product.builder()
+                .productName("test product")
+                .group(group)
+                .build();
+
+        User user = User.builder()
+                .userName("test user")
+                .build();
+
+        Cart cart = Cart.builder()
+                .user(user)
+                .build();
+
+        group.getProductList().add(product);
+        Group savedGroup = groupRepository.save(group);
+        user.cartList.add(cart);
+        cart.getProductList().add(product);
+        savedGroup.getProductList().get(0).getCartList().add(cart);
+        userRepository.save(user);
+        Cart cart1 = cartRepository.save(cart);
+
+        //When
+        productRepository.deleteAll();
+
+        //Then
+        assertEquals(1, userRepository.findAll().size());
+        assertEquals(1, cartRepository.findAll().size());
+        assertTrue(productRepository.findAll().isEmpty());
+        assertEquals(0, cartRepository.findAll().get(0).getProductList().size());
+    }
+
+    @Test
+    public void deletingCartDoesntDeleteProductNorUserButRemovesItFromProductsCartList() {
+        //Given
+        Group group = Group.builder()
+                .groupName("test group")
+                .build();
+
+        Product product = Product.builder()
+                .productName("test product")
+                .group(group)
+                .build();
+
+        User user = User.builder()
+                .userName("test user")
+                .build();
+
+        Cart cart = Cart.builder()
+                .user(user)
+                .build();
+
+        group.getProductList().add(product);
+        Group savedGroup = groupRepository.save(group);
+        user.cartList.add(cart);
+        cart.getProductList().add(product);
+        savedGroup.getProductList().get(0).getCartList().add(cart);
+        userRepository.save(user);
+        cartRepository.save(cart);
+
+        //When
+        cartRepository.deleteAll();
+
+        //Then
+        assertEquals(1, userRepository.findAll().size());
+        assertEquals(1, productRepository.findAll().size());
+        assertEquals(0, cartRepository.findAll().size());
+        assertEquals(0, productRepository.findAll().get(0).getCartList().size());
+    }
 }
