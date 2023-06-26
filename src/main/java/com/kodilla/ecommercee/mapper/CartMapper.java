e19c51ab1365f90cdad77713dcb3a81b4e2fb58e
Jan Mielziuk <jan.jakub.mielziuk@gmail.com>
2023-06-26 22:11:22 +0200

    CartMapper and CartController fixes

--- src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
@@ -1,18 +1,27 @@
 package com.kodilla.ecommercee.mapper;
 
+import com.kodilla.ecommercee.controller.exceptions.UserNotFoundException;
 import com.kodilla.ecommercee.domain.Cart;
 import com.kodilla.ecommercee.domain.dto.CartDto;
 import com.kodilla.ecommercee.domain.dto.UserDto;
+import com.kodilla.ecommercee.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
-
+@RequiredArgsConstructor
 @Service
 public class CartMapper {
-    public Cart mapToCart(final CartDto cartDto) {
-        return new Cart();
+    private final UserService userService;
+    public Cart mapToCart(final CartDto cartDto) throws UserNotFoundException {
+        return Cart.builder()
+                .user(userService.findUserById(cartDto.getUserId()))
+                .build();
     }
-    public CartDto mapToCartDto(final @NotNull Cart cartDto) {
-        return new CartDto(1L, new UserDto(1L, "1"));
+    public CartDto mapToCartDto(final Cart cart) {
+        return new CartDto(
+                cart.getCartId(),
+                cart.getUser().getUserId()
+        );
     }
 }
